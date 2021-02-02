23a614c911608daef71071e91dee8cb79d570744
samad-yar-khan <70485812+samad-yar-khan@users.noreply.github.com>
2021-02-03 04:22:41 +0530

    Install and setup EJS as the view engine

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const app = express();
 
 app.use('/' , require('./routes'));
 
+//set up the view engine
+app.set("view engine" , "ejs");
+app.set("views" , './views');
+
 
 app.listen(port , function(err){
     if(err){
