87928a29ef72fdc9c40d589c4d8f12a1a57d8466
samad-yar-khan <70485812+samad-yar-khan@users.noreply.github.com>
2021-02-03 04:42:07 +0530

    Delete the router and controller considering the small scall of the project for now

--- controllers/home_controller.js
+++
@@ -1,3 +0,0 @@
-module.exports.home = function(req,res){
-    return res.end("<h1>HELLO</h1>");
-}
--- index.js
+++ index.js
@@ -2,12 +2,23 @@ const express = require('express');
 const port = 8000;
 const app = express();
 
-app.use('/' , require('./routes'));
+const arr = [1,2,3,4,5];
+
+// app.use('/' , require('./routes'));
 
 //set up the view engine
 app.set("view engine" , "ejs");
 app.set("views" , './views');
 
+
+
+app.get('/' , module.exports.home = function(req,res){
+    return res.render( "home" , {
+        title:"TO DO APP",
+        heading : "TO DO APP",
+        nums : arr
+    });
+})
 
 app.listen(port , function(err){
     if(err){
