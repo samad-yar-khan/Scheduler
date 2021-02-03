e0a58a9e0d6664f527191d5e987ed61eb746bf17
samad-yar-khan <70485812+samad-yar-khan@users.noreply.github.com>
2021-02-04 04:57:28 +0530

    Make the structure of the html home page and add style to rhe home page

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,13 +1,45 @@
 
-const arr = [1,2,3,4,5];
+let months = ["JAN" ,"FEB" ,"MAR","APR" ,"MAY" , "JUNE" , "JUL" , "SEPT" , "OCT" ,"NOV" , "DEC"];
+
+let toDoList = [
+    {   
+        "id" : 1,
+        "desc" : "Workout !",
+        "category" : "Personal",
+        "date" : {
+            "day":"2",
+            "month":months[0],
+            "year": "2021"
+        }
+    },
+    {
+        "id" : 2,
+        "desc" : "Complete homework",
+        "category" : "School",
+        "date" : {
+            "day":"2",
+            "month":months[1],
+            "year":"2021"
+        }
+    },
+    {
+        "id" : 3,
+        "desc" : "Clean the house",
+        "category" : "Personal",
+        "date" : {
+            "day":"2",
+            "month":months[0],
+            "year":"1999"
+        }
+    }];
 
 
 module.exports.home = function(req , res){
 
     return res.render( "home" , {
         title:"TO DO APP",
-        heading : "TO DO APP",
-        nums : arr
+        heading : "Task Desk",
+        toDoList : toDoList
     });
 
 };
